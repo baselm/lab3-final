f81eb87ac3710c4b239e6366e2190d25c9e62be1
Basel Magableh <baseldit@Basels-MacBook-Pro.local>
2017-10-06 02:41:45 +0100

    add index.html

--- app.py
+++ app.py
@@ -5,25 +5,26 @@ mysql = MySQL()
 app = Flask(__name__)
 # My SQL Instance configurations 
 # Change the HOST IP and Password to match your instance configurations
+ 
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'P@ssw0rd'
 app.config['MYSQL_DB'] = 'studentbook'
 app.config['MYSQL_HOST'] = '35.195.195.114'
 mysql.init_app(app)
 
-@app.route('/hi/')
-@app.route('/hi/<name>')
+@app.route('/')
+@app.route('/<name>')
 def statichtml(name=None):
     return render_template('index.html', name=name)
 
 # The first route to access the webservice from http://35.190.217.3:5000/ 
 #@pp.route("/add") this will create a new endpoints that can be accessed using http://external-ip:5000/add
-@app.route("/")
+@app.route("/list")
 def hello(): # Name of the method
     cur = mysql.connection.cursor() #create a connection to the SQL instance
     cur.execute('''SELECT * FROM students''') # execute an SQL statment
     rv = cur.fetchall() #Retreive all rows returend by the SQL statment
-    return str(rv)      #Return the data in a string format
+    return render_template('index.html', name=str(rv))     #Return the data in a string format
 
 @app.route("/add/<name>/<email>")
 def add(name=None, email=None):
@@ -34,7 +35,7 @@ def add(name=None, email=None):
     data=(name,email)
     cur.execute(insert_stmt, data)
     mysql.connection.commit()
-    return "New Record is added to the database"
+    return render_template('index.html', name="New Record is added to the database")  
 
 @app.route("/update/<name>/<email>")
 def update(name=None, email=None):
@@ -45,16 +46,17 @@ def update(name=None, email=None):
     data=(name,email)
     cur.execute(update_stmt, data)
     mysql.connection.commit()
-    return "User recored was updated"      #Return the data in a string format
+    return render_template('index.html', name="User recored was updated")      #Return the data in a string format
 
 @app.route("/delete/<name>")
 def delete(name=None):
     cur=mysql.connection.cursor()
-    query = " DELETE FROM students WHERE studentName = %s "
-    data=(name)
-    cur.execute(query, data)
+    delstatmt = "DELETE FROM students WHERE studentName = ' {} ' ".format(name)
+    print(delstatmt)                
+   
+    cur.execute(delstatmt)
     mysql.connection.commit()
-    return "User recored was updated"      #Return the data in a string format
+    return render_template('index.html', name="User recored was deleted")      #Return the data in a string format
 
 if __name__ == "__main__":
         app.run(host='0.0.0.0', port='5000') 
